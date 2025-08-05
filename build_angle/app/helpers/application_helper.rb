8b14495dcffd080bb738e442b0cb03a2b8ccac42
PatrickOjiambo <pashrick237@gmail.com>
2025-08-05 22:21:32 +0300

    feat: v1 build angle

--- build_angle/app/helpers/application_helper.rb
+++ build_angle/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def status_color(status)
+  case status
+  when 'pending' then 'text-yellow-600'
+  when 'processing' then 'text-blue-600'
+  when 'complete' then 'text-green-600'
+  when 'failed' then 'text-red-600'
+  else 'text-gray-600'
+  end
+end
 end
